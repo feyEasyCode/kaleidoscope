a3976b99b4246f043824e3661b589e768cdff04d
zzf <ybmking@163.com>
2020-09-02 18:45:47 +0800

    create project ==> This is a great starting point！

---
+++ src/main/java/com/codingTool/controller/HomePageController.java
@@ -0,0 +1,28 @@
+package com.codingTool.controller;
+
+import com.codingTool.entity.HomeCommon;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/common")
+public class HomePageController extends BaseController {
+
+    @GetMapping("test")
+    public String Home(){
+        return "Welcome to Kaleidoscope！";
+    }
+
+    /**
+     * 页面通用数据获取
+     * @return
+     */
+    @GetMapping("/getCommonData")
+    public HomeCommon getCommonData(){
+        HomeCommon homeCommon = new HomeCommon();
+        homeCommon.setDescribe("欢迎来到万花筒项目，持续更新中，敬请期待~");
+        homeCommon.setUrl("www.uaike.com");
+        return homeCommon;
+    }
+
+
+}
