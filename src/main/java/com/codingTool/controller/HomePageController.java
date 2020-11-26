bfcbdbf7349b19d38378dc35cefcef29e2fb76a5
zzf <ybmking@163.com>
2020-11-26 18:47:17 +0800

    @Slf4j

--- src/main/java/com/codingTool/controller/HomePageController.java
+++ src/main/java/com/codingTool/controller/HomePageController.java
@@ -1,10 +1,14 @@
 package com.codingTool.controller;
 
 import com.codingTool.entity.HomeCommon;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/common")
+@Slf4j
+@RestController
+@RequestMapping("/common")
 public class HomePageController extends BaseController {
 
     @GetMapping("test")
@@ -21,6 +25,7 @@ public class HomePageController extends BaseController {
         HomeCommon homeCommon = new HomeCommon();
         homeCommon.setDescribe("欢迎来到万花筒项目，持续更新中，敬请期待~");
         homeCommon.setUrl("www.uaike.com");
+        log.info("加入log注解");
         return homeCommon;
     }
 
