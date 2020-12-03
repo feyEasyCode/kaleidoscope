cad4080dce257101969b8563678c57528b87aaed
zzf <ybmking@163.com>
2020-12-03 18:45:49 +0800

    add database connection etc.

--- src/main/java/com/codingTool/KaleidoscopeApplication.java
+++ src/main/java/com/codingTool/KaleidoscopeApplication.java
@@ -1,8 +1,10 @@
 package com.codingTool;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.codingTool.dao")
 @SpringBootApplication
 public class KaleidoscopeApplication {
 
