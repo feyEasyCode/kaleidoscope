96c32e3a6b051bcea53ac18f05aac6a229448ef9
zzf <ybmking@163.com>
2020-12-06 21:05:11 +0800

    create user model and add create method

---
+++ src/main/java/com/codingTool/entity/Customer.java
@@ -0,0 +1,67 @@
+package com.codingTool.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.IsAutoIncrement;
+import com.gitee.sunchenbin.mybatis.actable.annotation.IsKey;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @description:
+ * @author: zhangzhaofei
+ * @create: 2020-12-04
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class Customer {
+
+    @TableId(type = IdType.AUTO)//mybatis-plus主键注解
+    @IsAutoIncrement   //自增
+    @IsKey             //actable主键注解
+    @Column(comment = "用户ID")//对应数据库字段，不配置name会直接采用属性名作为字段名comment是注解
+    private Long id;
+
+    @Column(comment = "用户USER ID")
+    private String userId;
+
+    @Column(comment = "昵称")
+    private String name;
+
+    @Column(comment = "用户密码")
+    private String passWord;
+
+    @Column(comment = "手机号")
+    private String mobile;
+
+    @Column(comment = "邮箱")
+    private String email;
+
+    @Column(comment = "地址")
+    private String adress;
+
+    @Column(comment = "性别")
+    private String gender;
+
+    @Column(comment = "用户头像")
+    private String headImg;
+
+    @Column(comment = "用户类型")
+    private String userType;
+
+    @Column(comment = "账号状态")
+    private String userStatus;
+
+    @Column(comment = "创建时间")
+    private Date createTime;
+
+    @Column(comment = "修改时间")
+    private Date modifiedTime;
+}
